2cddcb04f38af5bc65e5810e92aed72cd55d1cb8
sudhakar <maddelasidhu@gmail.com>
2023-06-25 15:03:25 +0530

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -75,7 +75,7 @@ export default function Home() {
               Learn <span>-&gt;</span>
             </h2>
             <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!gfhkj
+              Learn about Next.js in an interactive course with&nbsp;quizzes!gfhkjsasas
             </p>
           </a>
 
