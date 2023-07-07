de30f24c24263613497cad196fa91eeaccd1e6d2
sudhakar <maddelasidhu@gmail.com>
2023-07-07 22:24:39 +0530

    Sixth Commit

---
+++ components/Myname.js
@@ -0,0 +1,8 @@
+export default function Myname() {
+    return (
+        <header className="hero__header">
+      <h1>Maddela Sudhakar</h1>
+      <h2>UI/UX Designer & Graphic Designer</h2>
+    </header>
+    )
+}
---
+++ components/ScrollSection.js
@@ -0,0 +1,60 @@
+import React, { useRef, useEffect } from "react";
+import Content from "@/components/Content";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+
+
+function ScrollSection() {
+  const sectionRef = useRef(null);
+  const triggerRef = useRef(null);
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    const pin = gsap.fromTo(
+      sectionRef.current,
+      {
+        translateX: 0,
+      },
+      {
+        translateX: "-300vw",
+        ease: "none",
+        duration: 1,
+        scrollTrigger: {
+          trigger: triggerRef.current,
+          start: "top top",
+          end: "2000 top",
+          scrub: 0.6,
+          pin: true,
+        },
+      }
+    );
+    return () => {
+      pin.kill();
+    };
+  }, []);
+
+  return (
+    <section className="scroll-section-outer">
+      <div ref={triggerRef}>
+        <div ref={sectionRef} className="scroll-section-inner">
+          <div className="scroll-section">
+            <Content />
+            <h3>Section 1</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 2</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 3</h3>
+          </div>
+          <div className="scroll-section">
+            <h3>Section 4</h3>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ScrollSection;
